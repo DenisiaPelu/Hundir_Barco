4a22f5844d5b42a8f399edcd7be8c20b30f6c1e6
DenisiaPelu <denisia_pelu@yahoo.es>
2024-09-26 17:41:11 +0200

    prueba_2

---
+++ main.py
@@ -0,0 +1,12 @@
+import proyecto_hundir_flota.Hundir_Barco.utils as utils
+
+juego=1
+
+while juego==1:
+    print("Buen venido al juego Hundir Barco")
+    print(input("Introduzca el numero de casillas del tablero"))
+
+    
+
+
+
